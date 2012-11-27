afdb4afeaa06b62af0ce0dfadfa17934d1ac860c
Ruchir Shukla <ruchir@techreceptives.com>
2012-11-27 23:53:08 +0530

    add new object signs and symptoms

---
+++ oemedical/oemedical_signs_and_symptoms/oemedical_signs_and_symptoms.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#/#############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.techreceptives.com>)
+#    Special Credit and Thanks to Thymbra Latinoamericana S.A.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#/#############################################################################
+from osv import osv
+from osv import fields
+
+
+class OeMedicalSignsAndSymptoms(osv.osv):
+    _name = 'oemedical.signs_and_symptoms'
+
+    _columns = {
+        'rec_name': fields.char(size=256, string='Name', required=True),
+        'clinical': fields.many2one('oemedical.pathology', string='Sign or Symptom', ),
+        'evaluation': fields.many2one('oemedical.patient.evaluation', string='Evaluation', ),
+        #'sign_or_symptom': fields.selection([], string='Subjective / Objective'),
+        'comments': fields.char(size=256, string='Comments', required=True),
+    }
+
+OeMedicalSignsAndSymptoms()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
