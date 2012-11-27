f0a664749edaea36f6749c34bfb04f1fc4cc9ea4
parthiv-patel <parthivgls@gmail.com>
2012-11-27 20:02:26 +0530

    added patient object

---
+++ oemedical/__openerp__.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.techreceptives.com>)
+#    Special Credit and Thanks to Thymbra Latinoamericana S.A.  
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+
+    'name' : 'OeMedical : Free Health and Hospital Information System',  
+    'version' : '1.0',
+    'author' : 'Tech-Receptives Solutions Pvt. Ltd.',
+    'category' : 'Generic Modules/Others',
+    'depends' : ['base','sale','purchase','account','product'],
+    'description' : """
+
+About OeMedical
+-------------
+OeMedical is a multi-user, highly scalable, centralized Electronic Medical Record (EMR) and Hospital Information System for openERP.
+
+OeMedical provides a free universal Health and Hospital Information System, so doctors and institutions all over the world, specially in developing countries will benefit from a centralized, high quality, secure and scalable system.
+
+
+OeMedical at a glance:
+
+
+    * Strong focus in family medicine and Primary Health Care
+
+    * Major interest in Socio-economics (housing conditions, substance abuse, education...)
+
+    * Diseases and Medical procedures standards (like ICD-10 / ICD-10-PCS ...)
+
+    * Patient Genetic and Hereditary risks : Over 4200 genes related to diseases (NCBI / Genecards)
+
+    * Epidemiological and other statistical reports
+
+    * 100% paperless patient examination and history taking
+
+    * Patient Administration (creation, evaluations / consultations, history ... )
+
+    * Doctor Administration
+
+    * Lab Administration
+
+    * Medicine / Drugs information (vademécum)
+
+    * Medical stock and supply chain management
+
+    * Hospital Financial Administration
+
+    * Designed with industry standards in mind
+
+    * Open Source : Licensed under AGPL 
+
+""",
+    "website" : "http://techreceptives.com",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml" : [],
+    "active": False 
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ oemedical/oemedical_patient/oemedical_patient.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#/#############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.techreceptives.com>)
+#    Special Credit and Thanks to Thymbra Latinoamericana S.A.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#/#############################################################################
+from osv import osv
+from osv import fields
+
+
+class OeMedicalPatient(osv.osv):
+    _name = 'oemedical.patient'
+
+    _columns = {
+        'family': fields.many2one('oemedical.family', string='Family', ),
+        'photo': fields.binary(string='Picture'),
+        #'sex': fields.selection([], string='Sex'),
+        #'blood_type': fields.selection([], string='Blood Type'),
+        'general_info': fields.text(string='General Information'),
+        'primary_care_doctor': fields.many2one('oemedical.physician', string='Primary Care Doctor', ),
+        'childbearing_age': fields.boolean(string='Potential for Childbearing'),
+        #'medications': fields.one2many('oemedical.patient.medication', 'relation_id', string='Medications', ),
+        'critical_info': fields.text(string='Important disease, allergy or procedures information'),
+        #'rh': fields.selection([], string='Rh'),
+        'current_address': fields.many2one('res.partner', string='Address', ),
+        #'diseases': fields.one2many('oemedical.patient.disease', 'relation_id', string='Diseases', ),
+        'lastname': fields.char(size=256, string='Lastname', required=True),
+        'ethnic_group': fields.many2one('oemedical.ethnicity', string='Ethnic group', ),
+        'ssn': fields.char(size=256, string='SSN', required=True),
+        #'vaccinations': fields.one2many('oemedical.vaccination', 'relation_id', string='Vaccinations', ),
+        'rec_name': fields.char(size=256, string='Name', required=True),
+        'name': fields.many2one('res.partner', string='Patient', ),
+        'dob': fields.date(string='DoB'),
+        'age': fields.char(size=256, string='Age', required=True),
+        #'marital_status': fields.selection([], string='Marital Status'),
+        'dod': fields.datetime(string='Date of Death'),
+        'current_insurance': fields.many2one('oemedical.insurance', string='Insurance', ),
+        'cod': fields.many2one('oemedical.pathology', string='Cause of Death', ),
+        'identification_code': fields.char(size=256, string='ID', required=True),
+        'deceased': fields.boolean(string='Deceased'),
+    }
+
+OeMedicalPatient()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
