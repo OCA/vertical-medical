97840fad9265cfdd65bd3d724871ffb0341cea24
Nhomar Hernandez <nhomar@gmail.com>
2012-12-01 23:53:06 -0530

    [ADD] Some files missed in last commit

---
+++ web_doc_oemedical/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'CMS',
+    'category': 'CMS',
+    'description':"""
+OeMedical Documentation management.
+===================================
+
+This module is to manage all documentation embebed on OeMedical.
+
+""",
+    'author': 'OeMedical Team',
+    'version': '0.1',
+    'depends': [],
+    'js': [],
+    'css': [],
+    'qweb' : [],
+    'auto_install': False,
+    'active': True,
+}
