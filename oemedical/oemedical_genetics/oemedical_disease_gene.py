05d7066adaf01450715732f27cef21c0cb0bd068
Federico Manuel Echeverri Choux <echeverrifm@gmail.com>
2013-05-08 20:08:29 -0500

    [REF] Change name of file for disease genetics

--- oemedical/oemedical_genetics/oemedical_disease_gene.py
+++ oemedical/oemedical_genetics/oemedical_disease_gene.py
@@ -19,7 +19,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #/#############################################################################
-import oemedical_disease_gene
+from osv import osv
+from osv import fields
 
 
+class OeMedicalDiseaseGene(osv.osv):
+    _name = 'oemedical.disease.gene'
+
+    _columns = {
+        'name': fields.char('Official Symbol', size=256, required=True),
+        'gene_id': fields.char('Gene ID', size=256, required=True),
+        'long_name': fields.char('Official Long Name', size=256, required=True),
+        'location': fields.char('Location', size=256, required=True, help="Locus of the chromosome"),
+        'chromosome': fields.char('Affected Chromosome', size=256, required=True),
+        'info': fields.text(string='Information'),
+        'dominance' : fields.selection([
+                    ('d', 'dominant'),
+                    ('r', 'recessive'),
+                    ], 'Dominance', select=True)
+
+    }
+
+OeMedicalDiseaseGene()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
