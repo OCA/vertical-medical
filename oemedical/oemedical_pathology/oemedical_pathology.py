07bd03183ae5063dcf9d4929ae79f48dbc9024f8
Federico Manuel Echeverri Choux <echeverrifm@gmail.com>
2013-06-10 12:48:46 -0500

    [IMP] Improve oemedical.pathology view

--- oemedical/oemedical_pathology/oemedical_pathology.py
+++ oemedical/oemedical_pathology/oemedical_pathology.py
@@ -27,25 +27,19 @@ class OeMedicalPathology(osv.Model):
     _name = 'oemedical.pathology'
 
     _columns = {
-        'category': fields.many2one('oemedical.pathology.category',
-                                    string='Main Category', 
+        'category': fields.many2one('oemedical.pathology.category', string='Main Category', 
               help='Select the main category for this disease This is usually'\
         'associated to the standard. For instance, the chapter on the ICD-10'\
         'will be the main category for de disease' ),
         'info': fields.text(string='Extra Info'),
-        'code': fields.char(size=256, string='Code', required=True, 
-                            help='Specific Code for the Disease (eg, ICD-10)'),
-        'name': fields.char(size=256, string='Name', required=True,
-                            translate=True, help='Disease name'),
-        'groups': fields.one2many('oemedical.disease_group.members',
-                                  'disease_group_id', string='Groups',
+        'code': fields.char(size=256, string='Code', required=True, help='Specific Code for the Disease (eg, ICD-10)'),
+        'name': fields.char(size=256, string='Name', required=True, translate=True, help='Disease name'),
+        'groups': fields.one2many('oemedical.disease_group.members', 'disease_group_id', string='Groups',
                      help='Specify the groups this pathology belongs. Some' \
                      ' automated processes act upon the code of the group' ),
-        'protein': fields.char(size=256, string='Protein involved', 
-                               help='Name of the protein(s) affected'),
+        'protein': fields.char(size=256, string='Protein involved', help='Name of the protein(s) affected'),
         'gene': fields.char(size=256, string='Gene'),
-        'chromosome': fields.char(size=256, string='Affected Chromosome', 
-                                  help='chromosome number'),
+        'chromosome': fields.char(size=256, string='Affected Chromosome', help='chromosome number'),
     }
 #    _sql_constraints = [
 #        ('name_uniq', 'UNIQUE(name)', 'Name must be unique!'),
