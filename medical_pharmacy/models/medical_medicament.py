0cbf782625fb5f6511d7072a025ea2c57f25184a
Dave Lasley <dave@laslabs.com>
2015-10-15 15:36:01 -0700

    Introduce medical_pharmacy concept

---
+++ medical_pharmacy/models/medical_medicament.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Dave Lasley <dave@laslabs.com>
+#    Copyright: 2015 LasLabs, Inc.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import fields, models
+
+
+class MedicalMedicament(models.Model):
+    _inherit = 'medical.medicament'
+    is_prescription = fields.Boolean(
+        string='Prescription Required',
+        help='Check this if a prescription is required for this medicament',
+    )
+    is_controlled = fields.Boolean(
+        string='Controlled Substance',
+        help='Check this if the medicament is a controlled substance',
+    )
