b7d7e4f47143875f41857f2d4de3461e5ed1f818
Dave Lasley <dave@laslabs.com>
2015-11-11 17:09:03 -0800

    Remove api import

--- medical_family/models/medical_patient.py
+++ medical_family/models/medical_patient.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class MedicalPatient(models.Model):
