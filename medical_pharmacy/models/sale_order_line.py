ff40ae7a1221ac3ecf2e3458e07d3cbe2a3b2fe3
Dave Lasley <dave@laslabs.com>
2015-12-14 17:31:59 -0800

    SMD-56 #time 151m #comment Add dispense qty calculation & basic order line validations

--- medical_pharmacy/models/sale_order_line.py
+++ medical_pharmacy/models/sale_order_line.py
@@ -19,11 +19,22 @@
 #
 ##############################################################################
 
-from openerp import fields, models
+from openerp import fields, models, _
+from openerp.exceptions import ValidationError
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    
+    @api.model
+    def _compute_dispense_qty(self, ):
+        rx_line = self.prescription_order_line_id
+        if self.product_uom == rx_line.dispense_uom_id:
+            return self.product_uom_qty
+        return proc_id.product_uom._compute_qty_obj(
+            self.product_uom_qty, rx_line.dispense_uom_id
+        )
+    
     patient_id = fields.Many2one(
         'medical.patient',
         related='order_id.patient_id',
@@ -35,3 +46,68 @@ class SaleOrderLine(models.Model):
         'product.product',
         related='prescription_order_line_id.product_id'
     )
+    dispense_qty = fields.Float(
+        default=0.0,
+        readonly=True,
+        compute='_compute_dispense_qty',
+    )
+    
+    @api.one
+    @api.constrains(
+        'product_id', 'prescription_order_line_id', 'patient_id',
+        'prescription_order_product_id',
+    )
+    def _check_sale_line_prescription(self, ):
+        '''
+        Validate whether the line can be dispensed based on Rx, pending
+        dispensings, etc.
+        :returns: bool -- If line can be processed
+        :raises: :class:`openerp.exceptions.ValidationError`
+        '''
+
+        if not product_id.is_medicament:
+            return True
+        if not product_id.is_prescription:
+            return True
+
+        rx_line = self.prescription_order_line_id
+
+        if self.patient_id != rx_line.patient_id:
+            raise ValidationError(_(
+                'Patients must be same on Order and Rx lines. '
+                'Got %s on order line %d, expected %s from rx line %d' % (
+                    self.patient_id.name, self.id,
+                    rx_line.patient_id.name, rx_line.id,
+                ),
+            ))
+        
+        if self.prescription_order_product_id != self.product_id:
+            if not self.is_substitutable:
+                raise ValidationError(_(
+                    'Products must be same on Order and Rx lines. '
+                    'Got %s on order line %d, expected %s from rx line %d' % (
+                        self.product_id.name, self.id,
+                        rx_line.product_id.name, rx_line.id,
+                    ),
+                ))
+            else:
+                raise NotImplementedError(_(
+                    'Drug substitution validation has not been implemented.'
+                ))
+
+        if not rx_line.can_dispense:
+            raise ValidationError(_(
+                'Cannot dispense - currently %f pending and %f exception.' % (
+                    rx_line.pending_dispense_qty,
+                    rx_line.exception_dispense_qty,
+                )
+            ))
+
+        if self.dispense_qty > rx_line.can_dispense_qty:
+            raise ValidationError(_(
+                'Cannot dispense - Order line %s goes over Rx qty by %d' % (
+                    self.name, self.dispense_qty - rx_line.can_dispense_qty
+                )
+            ))
+
+        return True
