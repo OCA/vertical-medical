53d928a118c8ff7804ee0e9f1de51786e9c8374f
Dave Lasley <dave@laslabs.com>
2016-01-15 20:22:54 -0800

    Disable installation due to known issues

--- medical_prescription_sale/__openerp__.py
+++ medical_prescription_sale/__openerp__.py
@@ -56,6 +56,6 @@
         'wizards/medical_prescription_to_sale_wizard_view.xml',
         'wizards/medical_sale_wizard_view.xml',
     ],
-    'installable': True,
+    'installable': False,
     'auto_install': False,
 }
