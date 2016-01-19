f7f1f8a5cff1ae9adde185ba5f5bc8e6ee20b395
Dave Lasley <dave@laslabs.com>
2016-01-18 21:47:55 -0800

    Fix example model

--- medical_base_history/models/medical_history_example.py
+++ medical_base_history/models/medical_history_example.py
@@ -22,7 +22,7 @@
 from openerp import models, fields
 
 
-class MedicalHistoryExample(models.Models):
+class MedicalHistoryExample(models.Model):
     '''
     This is an example of how to use the MedicalHistoryAbstract
 
