8b22686eb161cb3e85d6b6c4edf6925f5c7f5664
Dave Lasley <dave@laslabs.com>
2016-01-31 14:41:52 -0800

    Upgrade medical_pharmacy to v9 & add tests

--- medical_pharmacy/tests/test_medical_pharmacy.py
+++ medical_pharmacy/tests/test_medical_pharmacy.py
@@ -19,30 +19,32 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+import mock
+from openerp.tests.common import TransactionCase
+from openerp.addons.base.res.res_partner import res_partner
 
 
-class MedicalPharmacy(models.Model):
-    '''
-    Medical pharmacy attributes on res.partner
-    '''
-    _name = 'medical.pharmacy'
-    _description = 'Medical Pharmacy'
-    _inherits = {'res.partner': 'partner_id', }
+class TestMedicalPharmacy(TransactionCase):
 
-    partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        required=True,
-        ondelete='cascade',
-        index=True,
-    )
+    def setUp(self,):
+        super(TestMedicalPharmacy, self).setUp()
+        self.model_obj = self.env['medical.pharmacy']
+        self.vals = {
+            'name': 'Test Pharm',
+        }
 
-    @api.model
-    @api.returns('self', lambda value: value.id)
-    def create(self, vals):
-        vals['is_pharmacy'] = True
-        return super(MedicalPharmacy, self).create(vals)
+    def _new_record(self, ):
+        return self.model_obj.create(self.vals)
 
-    @api.multi
-    def onchange_state(self, state_id):
-        return self.partner_id.onchange_state(state_id)
+    def test_is_pharmacy(self, ):
+        ''' Validate is_pharmacy is set on partner '''
+        rec_id = self._new_record()
+        self.assertTrue(rec_id.is_pharmacy)
+
+    def test_onchange_state_passthru(self, ):
+        ''' Validate that onchange_state is passed thru to partner '''
+        rec_id = self._new_record()
+        with mock.patch.object(res_partner, 'onchange_state') as mk:
+            expect = 'Expect'
+            rec_id.onchange_state(expect)
+            mk.assert_called_once_with(expect)
