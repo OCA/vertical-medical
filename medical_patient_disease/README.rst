071cc0217cde0598eccaae28c81c24ac728cf241
Dave Lasley <dave@laslabs.com>
2016-01-31 14:30:48 -0800

    Move disease concept to patient namespace & upgrade to v9

--- medical_patient_disease/README.rst
+++ medical_patient_disease/README.rst
@@ -2,9 +2,9 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-===============
-Medical Disease
-===============
+=======================
+Medical Patient Disease
+=======================
 
 This module introduce the concept of
 
@@ -22,7 +22,7 @@ For further information, please visit:
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/159/8.0
+   :target: https://runbot.odoo-community.org/runbot/159/9.0
 
 
 Bug Tracker
@@ -31,7 +31,7 @@ Bug Tracker
 Bugs are tracked on `GitHub Issues <https://github.com/OCA/vertical-medical/issues>`_.
 In case of trouble, please check there if your issue has already been reported.
 If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/vertical-medical/issues/new?body=module:%20medical_disease%0Aversion:%208.0.1.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+`here <https://github.com/OCA/vertical-medical/issues/new?body=module:%20medical_patient_disease%0Aversion:%209.0.1.1.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 
 Credits
--- medical_patient_disease/models/medical_patient.py
+++ medical_patient_disease/models/medical_patient.py
@@ -26,17 +26,34 @@ from openerp import api, fields, models
 class MedicalPatient(models.Model):
     _inherit = 'medical.patient'
 
-    @api.one
-    def action_invalidate(self):
-        super(MedicalPatient, self).action_invalidate()
-        self.disease_ids.action_invalidate()
-
-    @api.one
-    def compute_count_disease_ids(self):
-        self.count_disease_ids = len(self.disease_ids)
-
     disease_ids = fields.One2many(
-        comodel_name='medical.patient.disease', inverse_name='patient_id',
-        string='Diseases')
+        comodel_name='medical.patient.disease',
+        inverse_name='patient_id',
+        string='Diseases',
+    )
     count_disease_ids = fields.Integer(
-        compute='compute_count_disease_ids', string='NB. Disease')
+        compute='compute_count_disease_ids',
+        string='Diseases',
+    )
+
+    @api.multi
+    def compute_count_disease_ids(self, ):
+        for rec_id in self:
+            rec_id.count_disease_ids = len(rec_id.disease_ids)
+
+    @api.multi
+    def action_invalidate(self, ):
+        for rec_id in self:
+            super(MedicalPatient, rec_id).action_invalidate()
+            rec_id.disease_ids.action_invalidate()
+
+    @api.multi
+    def action_revalidate(self, ):
+        for rec_id in self:
+            rec_id.active = True
+            rec_id.partner_id.active = True
+            disease_ids = self.env['medical.patient.disease'].search([
+                ('patient_id', '=', self.id),
+                ('active', '=', False),
+            ])
+            disease_ids.action_revalidate()
