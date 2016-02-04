55184a774cc8ed5a1162683ebdd64e0897355cec
Dave Lasley <dave@laslabs.com>
2016-02-04 14:25:47 -0800

    Fix headers, version & readme in _patient_disease per PR review

--- medical_patient_disease/__openerp__.py
+++ medical_patient_disease/__openerp__.py
@@ -1,30 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#     This file is part of medical_patient_disease,
-#     an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     medical_patient_disease is free software:
-#     you can redistribute it and/or modify it under the terms of the GNU
-#     Affero General Public License as published by the Free Software
-#     Foundation,either version 3 of the License, or (at your option) any
-#     later version.
-#
-#     medical_patient_disease is distributed
-#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#     PURPOSE.  See the GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the GNU Affero General Public License
-#     along with medical_patient_disease.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Medical Patient Disease',
-    'version': '9.0.2.0.0',
+    'version': '9.0.1.0.0',
     "author": "LasLabs, ACSONE SA/NV, Odoo Community Association (OCA)",
     "maintainer": "LasLabs, ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "http://www.acsone.eu",
