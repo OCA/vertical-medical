e313028dcfcf40f807ba4dc7520e00d41cabea56
Dave Lasley <dave@laslabs.com>
2016-02-04 14:47:14 -0800

    Fix headers, version & readme in _prescription per PR review

--- medical_prescription/models/__init__.py
+++ medical_prescription/models/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import medical_prescription_order
 from . import medical_prescription_order_line
