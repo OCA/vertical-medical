56f4e879008f136e52b68993fa1b11163e56e395
Dave Lasley <dave@laslabs.com>
2016-02-15 17:48:57 -0800

    Fix view and domain glitches in medical_prescription_disease

--- medical_prescription_disease/models/medical_patient_disease.py
+++ medical_prescription_disease/models/medical_patient_disease.py
@@ -6,12 +6,10 @@ from openerp import models, fields
 
 
 class MedicalPatientDisease(models.Model):
-    _inherit= 'medical.patient.disease'
+    _inherit = 'medical.patient.disease'
     prescription_order_line_ids = fields.One2many(
         string='Prescription Lines',
         comodel_name='medical.prescription.order.line',
         inverse_name='disease_id',
-        domain=lambda s: [('pathology_id', '=', s.pathology_id.id),
-                          ('patient_id', '=', s.patient_id.id), ],
         help='Prescriptions related to this disease.',
     )
