e186030af55410e4987c38a9cbfd444c88c5fa86
James Foster <jfoster@laslabs.com>
2016-03-09 13:47:19 -0500

    Add tests

---
+++ medical_prescription_thread/tests/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import test_prescription_order
+from . import test_prescription_order_line
---
+++ medical_prescription_thread/tests/test_prescription_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestPrescriptionOrder(TransactionCase):
+
+    def test_inherits_mail_thread(self):
+        model_obj = self.env['medical.prescription.order']
+        self.assertTrue(hasattr(model_obj, 'message_new'))
