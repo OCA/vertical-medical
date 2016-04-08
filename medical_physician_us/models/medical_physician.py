cf517f832f1b3e0999b16f18bb888a62737a7c1c
Dave Lasley <dave@laslabs.com>
2016-04-08 15:20:37 -0700

    Add physician NPI validation in medical_physician_us

--- medical_physician_us/models/medical_physician.py
+++ medical_physician_us/models/medical_physician.py
@@ -2,11 +2,13 @@
 # © 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import fields, models, api
+from openerp.exceptions import ValidationError
 
 
 class MedicalPhysician(models.Model):
-    _inherit = 'medical.physician'
+    _inherit = ['medical.physician', 'medical.luhn.abstract']
+    _name = 'medical.physician'
 
     license_num = fields.Char(
         string='State License #', help='State medical license #'
@@ -17,3 +19,12 @@ class MedicalPhysician(models.Model):
     npi_num = fields.Char(
         string='NPI #', help="National Provider Identifier"
     )
+
+    @api.multi
+    @api.constrains('country_id', 'npi_num')
+    def _check_ref(self):
+        """ Implement Luhns Formula to validate NPI """
+        for rec_id in self:
+            if rec_id.country_id.code == 'US':
+                if not self._luhn_is_valid(rec_id.npi_num):
+                    raise ValidationError('Invalid NPI Number.')
