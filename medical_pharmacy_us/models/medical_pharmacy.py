8d74edfa2ed44a37ada19d53cf72d10ecc707b21
Dave Lasley <dave@laslabs.com>
2016-04-08 14:21:43 -0700

    Add NPI validation for pharmacy in medical_pharmacy_us

--- medical_pharmacy_us/models/medical_pharmacy.py
+++ medical_pharmacy_us/models/medical_pharmacy.py
@@ -2,17 +2,29 @@
 # © 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import fields, models, api
+from openerp.exceptions import ValidationError
 
 
 class MedicalPharmacy(models.Model):
-    _inherit = 'medical.pharmacy'
-    nabp_num = fields.Integer(
+    _inherit = ['medical.pharmacy', 'medical.luhn.abstract']
+    _name = 'medical.pharmacy'
+
+    nabp_num = fields.Char(
         help='National Boards of Pharmacy Id #',
     )
-    medicaid_num = fields.Integer(
+    medicaid_num = fields.Char(
         help='Medicaid Id #',
     )
-    npi_num = fields.Integer(
+    npi_num = fields.Char(
         help="National Provider Identifier",
     )
+
+    @api.multi
+    @api.constrains('country_id', 'npi_num')
+    def _check_ref(self):
+        """ Implement Luhns Formula to validate NPI """
+        for rec_id in self:
+            if rec_id.country_id.code == 'US':
+                if not self._luhn_is_valid(rec_id.npi_num):
+                    raise ValidationError('Invalid NPI Number.')
---
+++ medical_pharmacy_us/tests/test_medical_pharmacy.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+from openerp.exceptions import ValidationError
+
+
+class TestMedicalPharmacy(TransactionCase):
+
+    def setUp(self,):
+        super(TestMedicalPharmacy, self).setUp()
+        self.model_obj = self.env['medical.pharmacy']
+        self.valid = [
+            4532015112830366,
+            6011514433546201,
+            6771549495586802,
+        ]
+        self.invalid = [
+            4531015112830366,
+            6011514438546201,
+            1771549495586802,
+        ]
+        self.country_id = self.env['res.country'].search([
+            ('code', '=', 'US'),
+        ],
+            limit=1
+        )
+
+    def _new_record(self, npi_num, country_id=None):
+        if not country_id:
+            country_id = self.country_id
+        return self.model_obj.create({
+            'name': 'Test Partner',
+            'npi_num': npi_num,
+            'country_id': country_id.id,
+        })
+
+    def test_valid_int(self):
+        for i in self.valid:
+            self.assertTrue(self._new_record(i))
+
+    def test_valid_str(self):
+        for i in self.valid:
+            self.assertTrue(self._new_record(str(i)))
+
+    def test_invalid_int(self):
+        for i in self.invalid:
+            with self.assertRaises(ValidationError):
+                self._new_record(i)
+
+    def test_invalid_str(self):
+        for i in self.invalid:
+            with self.assertRaises(ValidationError):
+                self._new_record(i)
+
+    def test_invalid_but_not_us(self):
+        country_id = self.env['res.country'].search([
+            ('code', '!=', 'US'),
+        ],
+            limit=1,
+        )
+        for i in self.invalid:
+            self.assertTrue(self._new_record(i, country_id))
