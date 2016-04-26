4584e2e063c0632e3c1adec50344feddf7ea719d
Dave Lasley <dave@laslabs.com>
2016-04-26 15:00:58 -0700

    Create new module medical_manufacturer

---
+++ medical_manufacturer/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+
+    'name': 'Medical Manufacturer',
+    'summary': 'This module adds the concept of a manufacturer on products.',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Medical',
+    'depends': [
+        'medical',
+    ],
+    "website": "https://laslabs.com",
+    "licence": "AGPL-3",
+    'installable': True,
+    'auto_install': False,
+}
