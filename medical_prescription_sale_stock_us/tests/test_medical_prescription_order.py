904de099b6acff6cee022edb91910760897d7d51
Dave Lasley <dave@laslabs.com>
2016-06-09 14:00:48 -0700

    [IMP] Remove unneeded code and add refill handling in medical_prescription_sale_stock_us

--- medical_prescription_sale_stock_us/tests/test_medical_prescription_order.py
+++ medical_prescription_sale_stock_us/tests/test_medical_prescription_order.py
@@ -7,8 +7,7 @@ from openerp import fields
 from openerp.exceptions import ValidationError
 
 
-class TestAll(TransactionCase):
-    ''' @TODO: Isolate all of this by model '''
+class TestMedicalPrescriptionOrder(TransactionCase):
 
     module = 'medical_prescription_sale_stock'
 
@@ -52,7 +51,7 @@ class TestAll(TransactionCase):
             'product_uom': 1,
         }
 
-    def _new_resources(self, clear=True):
+    def _new_resources(self, clear=True, refills=1):
         self.patient_id = self.env['medical.patient'].create(
             self.patient_vals
         )
@@ -73,6 +72,9 @@ class TestAll(TransactionCase):
             'medicament_id': self.medicament_id.id,
             'physician_id': self.physician_id.id,
             'patient_id': self.patient_id.id,
+            'duration': 1,
+            'duration_uom_id': self.env.ref('product.product_uom_day').id,
+            'refill_qty_original': refills,
         })
         self.rx_vals.update({
             'patient_id': self.patient_id.id,
@@ -117,119 +119,23 @@ class TestAll(TransactionCase):
         order_id = self.env['sale.order'].create(self.order_vals)
         return order_id
 
-    def _new_procurement(self, order_line_id):
+    def _new_procurement(self, order_line_id, date=None):
+        vals = order_line_id._prepare_order_line_procurement()
+        if date is not None:
+            vals['date_planned'] = date
         order_line_id.write({
             'procurement_ids': [(
-                0, 0, order_line_id._prepare_order_line_procurement()
+                0, 0, vals
             )],
         })
         return order_line_id.procurement_ids[-1]
 
-    def test_rx_line_compute_dispensings_cancelled(self, ):
+    def test_rx_line_compute_can_dispense_refill(self):
         self._new_procurement(
-            self._new_order().order_line[0]
-        ).state = 'cancel'
-        self.assertEqual(
-            1, self.rx_line_id.cancelled_dispense_qty,
-        )
-
-    def test_rx_line_compute_dispensings_pending_confirmed(self, ):
-        self._new_procurement(
-            self._new_order().order_line[0]
-        ).state = 'confirmed'
-        self.assertEqual(
-            1, self.rx_line_id.pending_dispense_qty,
-        )
-
-    def test_rx_line_compute_dispensings_pending_running(self, ):
-        self._new_procurement(
-            self._new_order().order_line[0]
-        ).state = 'running'
-        self.assertEqual(
-            1, self.rx_line_id.pending_dispense_qty,
-        )
-
-    def test_rx_line_compute_dispensings_done(self, ):
-        self._new_procurement(
-            self._new_order().order_line[0]
+            self._new_rx_order().order_line[0],
+            '2016-01-01 00:00:00',
         ).state = 'done'
-        self.assertEqual(
-            1, self.rx_line_id.dispensed_qty,
-        )
-
-    def test_rx_line_compute_dispensings_except(self, ):
-        self._new_procurement(
-            self._new_order().order_line[0]
-        ).state = 'exception'
-        self.assertEqual(
-            1, self.rx_line_id.exception_dispense_qty,
-        )
-
-    def test_rx_line_compute_can_dispense_none(self, ):
-        self._new_procurement(
-            self._new_order().order_line[0]
-        ).state
-        self.assertEqual(
-            0, self.rx_line_id.can_dispense_qty,
-        )
-        self.assertFalse(self.rx_line_id.can_dispense)
-
-    def test_rx_line_compute_can_dispense_one(self, ):
-        self._new_order()
         self.assertEqual(
             1, self.rx_line_id.can_dispense_qty,
         )
         self.assertTrue(self.rx_line_id.can_dispense)
-
-    def test_sale_line_compute_dispense_qty_identical(self, ):
-        order_id = self._new_rx_order()
-        self.assertEqual(
-            1, order_id.order_line[0].dispense_qty,
-        )
-
-    def test_sale_line_check_product(self, ):
-        order_id = self._new_rx_order()
-        order_line = order_id.order_line[0]
-        product = self.env['product.product'].search([
-            ('id', '!=', order_line.product_id.id)
-        ], limit=1)
-        with self.assertRaises(ValidationError):
-            order_line.product_id = product.id
-
-    def test_sale_line_check_product_substitute(self, ):
-        order_id = self._new_rx_order()
-        order_line = order_id.order_line[0]
-        self.rx_line_id.is_substitutable = True
-        product = self.env['product.product'].search([
-            ('id', '!=', order_line.product_id.id)
-        ], limit=1)
-        with self.assertRaises(ValidationError):
-            order_line.product_id = product.id
-
-    def test_prepare_order_line_procurement_otc(self, ):
-        order_id = self._new_order()
-        res = order_id.order_line[0]._prepare_order_line_procurement()
-        expect = self.env.ref(
-            '%s.route_warehouse0_otc' % self.module
-        )
-        res = res['route_ids'][0][2]
-        self.assertEquals(
-            [expect.id], res,
-            'Did not correctly assign route. Expect %s, Got %s' % (
-                [expect.id], res
-            )
-        )
-
-    def test_prepare_order_line_procurement_prescription(self, ):
-        order_id = self._new_rx_order()
-        res = order_id.order_line[0]._prepare_order_line_procurement()
-        expect = self.env.ref(
-            '%s.route_warehouse0_prescription' % self.module
-        )
-        res = res['route_ids'][0][2]
-        self.assertEquals(
-            [expect.id], res,
-            'Did not correctly assign route. Expect %s, Got %s' % (
-                [expect.id], res
-            )
-        )
