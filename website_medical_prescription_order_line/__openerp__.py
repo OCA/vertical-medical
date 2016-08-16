9a00cbf6f27a2ecafd126ddf674c635ca34a5c6b
Brett Wood <bwood@laslabs.com>
2016-08-15 22:44:53 -0700

    [FIX] website_medical_prescription_order_line: Add security for prescriptions, medicaments, and rxs.

--- website_medical_prescription_order_line/__openerp__.py
+++ website_medical_prescription_order_line/__openerp__.py
@@ -1,1 +1,28 @@
-# -*- coding: utf-8 -*-# © 2016 LasLabs Inc.# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).{    'name': 'Medical Website - Prescription Order Lines',    'version': '9.0.1.0.0',    'author': "LasLabs, Odoo Community Association (OCA)",    'category': 'Medical',    'depends': [        'website_sale',        'website_medical_patient',        'medical_prescription',        'medical_prescription_sale',        'medical_prescription_sale_stock',    ],    "website": "https://laslabs.com",    "license": "AGPL-3",    "data": [        'views/assets.xml',        'views/website_medical_template.xml',        'views/medical_prescription_order_line_template.xml',        'security/medical_prescription_order_security.xml',        'security/ir.model.access.csv',    ],    "application": False,    'installable': True,}
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Medical Website - Prescription Order Lines',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Medical',
+    'depends': [
+        'website_sale',
+        'website_medical_patient',
+        'medical_prescription',
+        'medical_prescription_sale',
+        'medical_prescription_sale_stock',
+    ],
+    "website": "https://laslabs.com",
+    "license": "AGPL-3",
+    "data": [
+        'views/assets.xml',
+        'views/website_medical_template.xml',
+        'views/medical_prescription_order_line_template.xml',
+        'security/medical_prescription_order_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    "application": False,
+    'installable': True,
+}
