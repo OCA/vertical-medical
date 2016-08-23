244b60e33070660243bea82c53a2bf30e02a11e5
Brett Wood <bwood@laslabs.com>
2016-08-23 16:34:32 -0700

    [ADD] sale_medical_medicament: Apply medicament context to sale views * Move display_name logic from website_sale_medical_medicament * Create sale_medical_medicament

--- sale_medical_medicament/tests/__init__.py
+++ sale_medical_medicament/tests/__init__.py
@@ -2,4 +2,5 @@
 # © 2016-TODAY LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import medical_medicament
+from . import test_product_product
+from . import test_product_template
--- website_sale_medical_medicament/__openerp__.py
+++ website_sale_medical_medicament/__openerp__.py
@@ -1,1 +1,22 @@
-# -*- coding: utf-8 -*-# © 2016 LasLabs Inc.# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).{    'name': 'Medical Website - Medicament Sales',    'version': '9.0.1.0.0',    'author': "LasLabs, Odoo Community Association (OCA)",    'category': 'Medical',    'depends': [        'medical_prescription_sale_stock',        'website_sale',    ],    "website": "https://laslabs.com",    "license": "AGPL-3",    "application": False,    'installable': True,    'data': [        'security/ir.model.access.csv',    ],}
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Medical Website - Medicament Sales',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Medical',
+    'depends': [
+        'medical_prescription_sale_stock',
+        'website_sale',
+        'sale_medical_medicament',
+    ],
+    "website": "https://laslabs.com",
+    "license": "AGPL-3",
+    "application": False,
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
