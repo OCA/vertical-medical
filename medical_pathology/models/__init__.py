1591f59e3f04ab075673f6ba2cf14abfea15c6fc
Dave Lasley <dave@laslabs.com>
2016-10-13 15:24:09 -0700

    [ADD] medical_pathology: Create medical_pathology.

---
+++ medical_pathology/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import medical_pathology
+from . import medical_pathology_group
+from . import medical_pathology_category
+from . import medical_pathology_code_type
---
+++ medical_pathology/tests/test_medical_pathology.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests.common import TransactionCase
+from psycopg2 import IntegrityError
+
+
+class TestMedicalPathology(TransactionCase):
+
+    def setUp(self,):
+        super(TestMedicalPathology, self).setUp()
+        self.model_obj = self.env['medical.pathology']
+        self.code_type_id = self.env['medical.pathology.code.type'].create({
+            'name': 'Code Type',
+        })
+        self.vals = {
+            'name': 'Test Pathology',
+            'code': 'TESTPATH',
+            'code_type_id': self.code_type_id.id,
+        }
+        self.record_id = self._test_record()
+
+    def _test_record(self, ):
+        return self.model_obj.create(self.vals)
+
+    def test_check_unique_code(self, ):
+        with self.assertRaises(IntegrityError):
+            self._test_record()
