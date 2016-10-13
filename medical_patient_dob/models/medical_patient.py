7178263281f0a4248900b57a3fc520620488ea1b
Dave Lasley <dave@laslabs.com>
2016-10-13 15:19:10 -0700

    [ADD] medical_patient_dob: Create medical_patient_dob.

--- medical_patient_dob/models/medical_patient.py
+++ medical_patient_dob/models/medical_patient.py
@@ -9,19 +9,17 @@ class MedicalPatient(models.Model):
     _inherit = 'medical.patient'
 
     @api.multi
-    def _format_birthdate_date(self):
+    def _format_dob(self, ):
         self.ensure_one()
         date = 'No DoB'
-        if self.birthdate_date:
-            date = fields.Datetime.from_string(self.birthdate_date).strftime(
-                '%m/%d/%Y'
-            )
+        if self.dob:
+            date = fields.Datetime.from_string(self.dob).strftime('%m/%d/%Y')
         return ' [%s]' % date
 
     @api.multi
-    def name_get(self):
+    def name_get(self, ):
         res = []
         for rec_id in self:
-            name = '%s%s' % (rec_id.name, rec_id._format_birthdate_date())
+            name = '%s%s' % (rec_id.name, rec_id._format_dob())
             res.append((rec_id.id, name))
         return res
