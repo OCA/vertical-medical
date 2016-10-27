016cf5a729db5a23ff0e12028a75475631dcdaff
Brett Wood <bwood@laslabs.com>
2016-10-27 13:58:42 -0700

    medical_physician: Improve module to match oca conventions * Upgrade readme to match oca template * Fix copyright in headers * Remove medical_physician_service * Remove medical_physician_schedule_template * Remove yml tests * Refactor tests to use demo data * Improve existing demo data * Remove superfluous fields * Improve medical_specialty data id

--- medical_physician/tests/test_medical_physician.py
+++ medical_physician/tests/test_medical_physician.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
@@ -7,21 +7,35 @@ from openerp.tests.common import TransactionCase
 
 class TestMedicalPhysician(TransactionCase):
 
-    def setUp(self, ):
+    def setUp(self):
         super(TestMedicalPhysician, self).setUp()
-        vals = {
-            'name': 'Test Specialty',
-            'code': 'TS',
-        }
-        specialty_id = self.env['medical.specialty'].create(vals)
-        vals = {
-            'name': 'Test Physician',
-            'specialty_id': specialty_id.id,
-        }
-        self.physician_id = self.env['medical.physician'].create(vals)
+        self.physician_id = self.env.ref(
+            'medical_physician.medical_physician_physician_1'
+        )
 
-    def test_is_doctor(self, ):
-        self.assertTrue(self.physician_id.is_doctor)
+    def test_is_doctor(self):
+        """ Test physician is doctor """
+        self.assertTrue(
+            self.physician_id.is_doctor,
+            'Should be True.\rGot: %s\rExpected: %s' % (
+                self.physician_id.is_doctor, True
+            )
+        )
 
-    def test_sequence(self, ):
-        self.assertTrue(self.physician_id.code)
+    def test_sequence(self):
+        """ Test physician code is set to sequence next_by_code if None """
+        self.assertTrue(
+            self.physician_id.code,
+            'Code should be next_by_code if None.\rGot: %s\rExpected: %s' % (
+                self.physician_id.code, 'Truthey value'
+            )
+        )
+
+    def test_physician_customer(self):
+        """ Test customer set to False when creating physician """
+        self.assertFalse(
+            self.physician_id.customer,
+            'Customer should be False.\rGot: %s\rExpected: %s' % (
+                self.physician_id.code, False
+            )
+        )
