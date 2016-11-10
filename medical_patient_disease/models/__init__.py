c3b90cb4ae089b382ca4b2b851dae04580a8f59b
Dave Lasley <dave@laslabs.com>
2016-11-10 01:51:56 -0800

    [ADD] medical_patient_disease: Create medical_patient_disease.

---
+++ medical_patient_disease/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import medical_patient_disease
+from . import medical_patient
