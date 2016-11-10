791d7597ccc98e974e4d5ed04e0a28b06409cd28
Brett Wood <bwood@laslabs.com>
2016-11-10 03:07:56 -0800

    [IMP] medical_patient_disease: Improve module to match oca conventions. * Add demo data * Refactor tests to use demo data * Fix copyright in headers * Fix view ids * Add oca icon to description * Fix readme to match oca template * Reorganize attributes in manifest * Add string attribute to date fields. * Improve code readability in models * Remove duplicate pathology_id demo data field * Add license to manifest file * Change compute_name to _compute_name * Remove duplicate name from Readme

--- medical_patient_disease/models/medical_patient_disease.py
+++ medical_patient_disease/models/medical_patient_disease.py
@@ -1,69 +1,79 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class MedicalPatientDisease(models.Model):
     _name = 'medical.patient.disease'
     _description = 'Medical Patient Disease'
 
-    name = fields.Char(compute='_compute_name', store=True)
     treatment_description = fields.Char()
-    expire_date = fields.Datetime(compute='_compute_expire_date', store=True)
     short_comment = fields.Char()
+    name = fields.Char(
+        compute='_compute_name',
+        store=True,
+    )
+    expire_date = fields.Datetime(
+        string='Date Expired',
+        compute='_compute_expire_date',
+        store=True,
+    )
     pathology_id = fields.Many2one(
         string='Pathology',
         comodel_name='medical.pathology',
         index=True,
         required=True,
-        help='Pathology (disease type) that patient was diagnosed with.',
+        help='Pathology (disease type) the patient was diagnosed with.',
     )
     physician_id = fields.Many2one(
         string='Physician',
         comodel_name='medical.physician',
         index=True,
-        help='Physician that diagnosed this disease.'
+        help='Physician that diagnosed this disease.',
     )
     patient_id = fields.Many2one(
         string='Patient',
         comodel_name='medical.patient',
         required=True,
         index=True,
-        help='Patient that was diagnosed with this disease.'
+        help='Patient that was diagnosed with this disease.',
     )
-    disease_severity = fields.Selection([
-        ('1_mi', 'Mild'),
-        ('2_mo', 'Moderate'),
-        ('3_sv', 'Severe')
-    ],
+    disease_severity = fields.Selection(
         string='Severity',
+        selection=[
+            ('1_mi', 'Mild'),
+            ('2_mo', 'Moderate'),
+            ('3_sv', 'Severe'),
+        ],
         help='Level of severity of this disease.',
     )
-    state = fields.Selection([
-        ('a', 'Acute'),
-        ('c', 'Chronic'),
-        ('u', 'Unchanged'),
-        ('h', 'Healed'),
-        ('i', 'Improving'),
-        ('w', 'Worsening'),
-    ],
+    state = fields.Selection(
         string='Status',
+        selection=[
+            ('a', 'Acute'),
+            ('c', 'Chronic'),
+            ('u', 'Unchanged'),
+            ('h', 'Healed'),
+            ('i', 'Improving'),
+            ('w', 'Worsening'),
+        ],
         help='Status of this disease.',
     )
-    allergy_type = fields.Selection([
-        ('da', 'Drug Allergy'),
-        ('fa', 'Food Allergy'),
-        ('ma', 'Misc Allergy'),
-        ('mc', 'Misc Contraindication'),
-    ],
+    allergy_type = fields.Selection(
+        selection=[
+            ('da', 'Drug Allergy'),
+            ('fa', 'Food Allergy'),
+            ('ma', 'Misc Allergy'),
+            ('mc', 'Misc Contraindication'),
+        ],
         help='If the disease is an allergy, indicate the type.',
     )
     weeks_of_pregnancy = fields.Integer(
         string='Pregnancy Week #',
-        help='Indicate how far along the pregnancy was when patient was'
-        ' diagnosed with this disease.',
+        help='Indicate how far along the pregnancy was'
+             ' when patient was diagnosed with this disease.',
     )
     age = fields.Integer(
         string='Age When Diagnosed',
@@ -75,38 +85,43 @@ class MedicalPatientDisease(models.Model):
     )
     is_infectious = fields.Boolean(
         string='Infectious Disease',
-        help='Check this box to indicate that the disease is likely to be'
-        ' transmitted.',
+        help='Check this box to indicate that the'
+             ' disease is likely to be transmitted.',
     )
     is_allergy = fields.Boolean(
         string='Allergic Disease',
-        help='Check this box to indicate that the disease is an allergy.',
+        help='Check this box to indicate that the'
+             ' disease is an allergy.',
     )
     is_pregnant = fields.Boolean(
         string='Pregnancy Warning',
-        help='Check this box to indicate that the patient was pregnant at'
-        ' the time of disease diagnosis.'
+        help='Check this box to indicate that the patient'
+             ' was pregnant at the time of disease diagnosis.',
     )
     is_on_treatment = fields.Boolean(
         string='Currently on Treatment',
-        help='Check this box if the patient is currently receiving treatment'
-        ' for this disease',
+        help='Check this box if the patient is currently'
+             ' receiving treatment for this disease.',
     )
     treatment_start_date = fields.Date(
-        help='If the patient is receiving treatment, document the start date'
-        ' here.',
+        string="Date Treatment Starts",
+        help='If the patient is receiving treatment'
+             ' state the start date here.',
     )
     treatment_end_date = fields.Date(
-        help='If the patient is/was receiving treament, document the end date'
-        ' here.',
+        string='Date Treatment Ends',
+        help='If the patient is/was receiving treament'
+             ' state the end date here.',
     )
     diagnosed_date = fields.Date(
         string='Date of Diagnosis',
-        help='Document when the patient was diagnosed with this disease.',
+        help='When the patient was diagnosed'
+             ' with this disease.',
     )
     healed_date = fields.Date(
         string='Date of Healing',
-        help='Document when the patient was fully relieved of this disease.',
+        help='When the patient was fully relieved'
+             ' of this disease.',
     )
     notes = fields.Text(
         help='Any additional information that may be helpful.',
