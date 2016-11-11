1d17da2c729972b81385aa7f5846f8e4975503ec
Brett Wood <bwood@laslabs.com>
2016-11-10 19:31:37 -0800

    [IMP] medical_patient_disease_allergy: Improve module to match oca conventions. * Fix readme to match oca template * Fix copyright in headers * Fix view ids * Reorganize attributes in manifest * Add demo data * Refactor tests to use demo data * Remove svg icon * Remove maintainer attribute from manifest * Fix attribute alignment in medical_patient_view

--- medical_patient_disease_allergy/tests/__init__.py
+++ medical_patient_disease_allergy/tests/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import common
 from . import test_medical_patient
 from . import test_medical_patient_disease
--- medical_patient_disease_allergy/tests/test_medical_patient_disease.py
+++ medical_patient_disease_allergy/tests/test_medical_patient_disease.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from .common import CommonTestBase
+from openerp.tests.common import TransactionCase
 
 
-class TestMedicalPatientDisease(CommonTestBase):
+class TestMedicalPatientDisease(TransactionCase):
+
+    def setUp(self):
+        super(TestMedicalPatientDisease, self).setUp()
+        self.disease_2 = self.env.ref(
+            'medical_patient_disease.medical_patient_disease_disease_2'
+        )
+        self.disease_3 = self.env.ref(
+            'medical_patient_disease_allergy.medical_patient_disease_disease_3'
+        )
 
     def test_compute_is_allergy(self):
-        """ It should be identified as an allergy """
+        """ Test is_allergy True if pathology code_type is allergy """
         self.assertTrue(
-            self.disease_id.is_allergy,
+            self.disease_3.is_allergy,
+            'is_allergy should be True.\rGot: %s\rExpected: %s' % (
+                self.disease_3.is_allergy, True
+            )
+        )
+
+    def test_compute_is_allergy_not_allergy(self):
+        """ Test is_allergy not incorrectly set to True """
+        self.assertFalse(
+            self.disease_2.is_allergy,
+            'is_allergy should be True.\rGot: %s\rExpected: %s' % (
+                self.disease_2.is_allergy, False
+            )
         )
