60829cd7f031c3f745fd32d09649eff11123a28c
Brett Wood <bwood@laslabs.com>
2016-12-12 15:57:14 -0800

    [IMP] mail_thread_medical_prescription: Improve to match oca conventions. * Improve test error messaging * Fix inherit ids in order and order_line views * Reorganize manifest attributes * Add oca icon to description * Fix readme to match oca template * Fix copyrights in headers * Rename module to mail_thread_medical_prescription

--- mail_thread_medical_prescription/tests/__init__.py
+++ mail_thread_medical_prescription/tests/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import test_prescription_order
 from . import test_prescription_order_line
