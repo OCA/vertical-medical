deeb43f537c2afc392291e63668719b4f9c7b676
Dave Lasley <dave@laslabs.com>
2016-12-21 18:49:06 -0800

    [ADD] medical_prescription_sale_crm: Add module.

---
+++ medical_prescription_sale_crm/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Medical Prescription Sale CRM',
+    'description': 'Create Opportunities from Prescriptions',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'category': 'Medical',
+    'depends': [
+        'sale_crm',
+        'medical_prescription_sale',
+    ],
+    "website": "https://laslabs.com",
+    "license": "AGPL-3",
+    "data": [
+        'wizards/medical_lead_wizard_view.xml',
+        'views/crm_lead_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
