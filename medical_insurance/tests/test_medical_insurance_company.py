3b0e74dc6546ad8d347d03dd93fc773c9aea7de2
Dave Lasley <dave@laslabs.com>
2016-12-28 17:46:34 -0800

    [10.0]IMP] medical_insurance: Implement abstract entity * Implement medical abstract entity for medical insurance company

--- medical_insurance/tests/test_medical_insurance_company.py
+++ medical_insurance/tests/test_medical_insurance_company.py
@@ -2,7 +2,6 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import mock
 from openerp.tests.common import TransactionCase
 
 
@@ -22,11 +21,3 @@ class TestMedicalInsuranceCompany(TransactionCase):
         ''' Validate is_insurance_company is set on partner '''
         rec_id = self._new_record()
         self.assertEqual(rec_id.type, 'medical.insurance.company')
-
-    def test_onchange_state_passthru(self, ):
-        ''' Validate that onchange_state is passed thru to partner '''
-        rec_id = self._new_record()
-        with mock.patch.object(rec_id, 'onchange_state') as mk:
-            expect = 'Expect'
-            rec_id.onchange_state(expect)
-            mk.assert_called_once_with(expect)
