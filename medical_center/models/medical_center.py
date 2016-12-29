838b4bc7da93d631358801df0f80a05ed7dbc419
Dave Lasley <dave@laslabs.com>
2016-12-28 17:17:44 -0800

    [IMP] medical_center: Implement abstract entity * Implement medical abstract entity for medical center

--- medical_center/models/medical_center.py
+++ medical_center/models/medical_center.py
@@ -2,26 +2,18 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class MedicalCenter(models.Model):
     _name = 'medical.center'
     _description = 'Medical Center'
-    _inherits = {'res.partner': 'partner_id'}
-
-    partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        required=True,
-        ondelete='cascade',
-        index=True,
-    )
+    _inherit = 'medical.abstract.entity'
 
     @api.model
-    def create(self, vals):
+    def _create_vals(self, vals):
         vals.update({
             'is_company': True,
             'customer': False,
-            'type': self._name,
         })
-        return super(MedicalCenter, self).create(vals)
+        return super(MedicalCenter, self)._create_vals(vals)
