ef649292fcb3b514806447fe89359ed312c0e74b
Dave Lasley <dave@laslabs.com>
2016-12-29 23:07:29 -0800

    [ADD] medical_manufacturer: Add module.

---
+++ medical_manufacturer/tests/test_medical_manufacturer.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestMedicalManufacturer(TransactionCase):
+
+    def setUp(self,):
+        super(TestMedicalManufacturer, self).setUp()
+        self.model_obj = self.env['medical.manufacturer']
+        self.vals = {
+            'name': 'Test Pharm',
+        }
+
+    def _new_record(self, ):
+        return self.model_obj.create(self.vals)
+
+    def test_is_manufacturer(self, ):
+        ''' Validate is_manufacturer is set on partner '''
+        rec_id = self._new_record()
+        self.assertTrue(rec_id.is_manufacturer)
