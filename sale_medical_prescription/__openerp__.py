899f598690a00bdce57e3e77bb702cb50735a6e5
Brett Wood <bwood@laslabs.com>
2016-12-30 04:26:27 -0800

    [FIX] sale_medical_prescription: Trim trailing xml whitespace.

--- sale_medical_prescription/__openerp__.py
+++ sale_medical_prescription/__openerp__.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-
     "name": "Medical Prescription Sales",
     "summary": "Create sale orders from prescriptions.",
     "version": "9.0.1.0.0",
