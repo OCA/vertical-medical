122bfbe3ccba311e69578eda93bc6d0b0fc0fa7e
Dave Lasley <dave@laslabs.com>
2017-01-03 13:21:08 -0800

    [IMP] medical_center: Add default image and attributes * Add default image * Add mode=primary

--- medical_center/models/medical_center.py
+++ medical_center/models/medical_center.py
@@ -2,7 +2,8 @@
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import api, models, tools
+from odoo.modules import get_module_resource
 
 
 class MedicalCenter(models.Model):
@@ -17,3 +18,15 @@ class MedicalCenter(models.Model):
             'customer': False,
         })
         return super(MedicalCenter, self)._create_vals(vals)
+
+    @api.model
+    def _get_default_image(self, vals):
+        res = super(MedicalCenter, self)._get_default_image(vals)
+        if not res:
+            return res
+        img_path = get_module_resource(
+            'medical_center', 'static/src/img', 'medical-center-avatar.png',
+        )
+        with open(img_path, 'r') as image:
+            base64_image = image.read().encode('base64')
+            return tools.image_resize_image_big(base64_image)
