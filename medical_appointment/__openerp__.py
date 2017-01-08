c6296256a5df5fb718c9f199f29c97eb8bbd3aa7
Dave Lasley <dave@laslabs.com>
2017-01-07 19:30:58 -0800

    [MIG] medical_appointment: Upgrade to v9

---
+++ medical_appointment/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+
+    'name': 'Medical Appointment',
+    'summary': 'Add Appointment concept to medical_physician',
+    'version': '9.0.1.0.0',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'category': 'Medical',
+    'depends': [
+        'medical_base_history',
+        'medical_physician',
+    ],
+    'data': [
+        'views/medical_appointment_view.xml',
+        'views/medical_menu.xml',
+        'security/ir.model.access.csv',
+        'security/medical_security.xml',
+        'data/medical_appointment_data.xml',
+        'data/medical_appointment_sequence.xml',
+    ],
+    'website': 'https://laslabs.com',
+    'licence': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
