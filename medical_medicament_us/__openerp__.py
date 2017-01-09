129c926613830eed991ef618c0258de2263e3f60
Brett Wood <bwood@laslabs.com>
2017-01-09 12:32:31 -0800

    [IMP] medical_medicament_us: Various small to medium fixes. * Set _us fields in medicament tree view to invisible. (crowding the view) * Reorganize attributes in manifest * Fix readme to match oca template * Add demo data * Refactor tests to use demo data * Add oca icon to description * Small fixes to models * Grammar error fixes in readme * Fix control_code help attr in medical_medicament * Remove medical_medication dependency * Set control_code back to visible in medical_medicament_view * Set gcn_id back to visible in medical_medicament_view * Add more detailed help to control_code field * Minor improvements to demo data

--- medical_medicament_us/__openerp__.py
+++ medical_medicament_us/__openerp__.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-
-    'name': 'Medical Medicament - US Locale',
-    'version': '9.0.1.0.0',
-    'author': "LasLabs, Odoo Community Association (OCA)",
-    'category': 'Medical',
-    'depends': [
-        'medical_base_us',
-        'medical_medication',
+    "name": "Medical Medicament - US Locale",
+    "summary": "Extension of medical_medicament that provides US locale.",
+    "version": "9.0.1.0.0",
+    "category": "Medical",
+    "website": "https://laslabs.com",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "medical_base_us",
+        "medical_manufacturer",
     ],
-    'website': 'https://laslabs.com',
-    'license': 'AGPL-3',
-    'data': [
-        'views/medical_medicament_view.xml',
-        'security/ir.model.access.csv',
+    "data": [
+        "views/medical_medicament_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
-    'auto_install': True,
+    "demo": [
+        "demo/medical_manufacturer_demo.xml",
+        "demo/medical_medicament_gcn_demo.xml",
+        "demo/medical_medicament_demo.xml",
+        "demo/medical_medicament_ndc_demo.xml",
+    ],
 }
--- medical_medicament_us/tests/test_medical_medicament.py
+++ medical_medicament_us/tests/test_medical_medicament.py
@@ -10,61 +10,56 @@ class TestMedicalMedicament(TransactionCase):
     def setUp(self):
         super(TestMedicalMedicament, self).setUp()
 
-        self.test_gcn = self.env['medical.medicament.gcn'].create({})
-        self.test_drug_form = self.env.ref('medical_medicament.AEM')
-
-    def _new_medicament(self, extra_values):
-        base_values = {
-            'drug_form_id': self.test_drug_form.id,
-            'name': 'Test Medicament',
-            'gcn_id': self.test_gcn.id,
-        }
-        base_values.update(extra_values)
-
-        return self.env['medical.medicament'].create(base_values)
+        self.advil_us_1 = self.env.ref(
+            'medical_medicament_us.medical_medicament_advil_us_1'
+        )
+        self.gcn_demo_us_2 = self.env.ref(
+            'medical_medicament_us.medical_medicament_gcn_us_2'
+        )
 
     def test_compute_brand_ids_no_gcn_id(self):
-        '''It should return an empty recordset for medicaments without a GCN'''
-        test_medicament = self._new_medicament({'gcn_id': None})
-
-        self.assertFalse(test_medicament.brand_ids)
+        """ Test returns an empty recordset for medicaments without a GCN """
+        self.advil_us_1.gcn_id = None
+        self.assertEquals(
+            len(self.advil_us_1.brand_ids),
+            0,
+        )
 
     def test_compute_brand_ids_no_matches(self):
-        '''It should return empty recordset when there are no brand variants'''
-        test_medicament = self._new_medicament({'gpi': '1'})
-
-        self.assertFalse(test_medicament.brand_ids)
+        """ Test returns empty recordset when there are no brand variants """
+        self.advil_us_1.gpi = '1'
+        self.assertEquals(
+            len(self.advil_us_1.brand_ids),
+            0,
+        )
 
     def test_compute_brand_ids_valid_matches(self):
-        '''It should return all matching medicaments, including self'''
-        test_medicament = self._new_medicament({'gpi': '2'})
-        test_medicament_2 = self._new_medicament({'gpi': '2'})
-        self._new_medicament({'gpi': '1'})
-
-        self.assertEqual(
-            test_medicament.brand_ids.ids,
-            [test_medicament.id, test_medicament_2.id],
+        """ Test returns all matching medicaments, including self """
+        self.assertEquals(
+            len(self.advil_us_1.brand_ids),
+            1,
         )
 
     def test_compute_generic_ids_no_gcn_id(self):
-        '''It should return an empty recordset for medicaments without a GCN'''
-        test_medicament = self._new_medicament({'gcn_id': None})
-
-        self.assertFalse(test_medicament.generic_ids)
+        """ Test returns an empty recordset for medicaments without a GCN """
+        self.advil_us_1.gpi = '1'
+        self.advil_us_1.gcn_id = None
+        self.assertEquals(
+            len(self.advil_us_1.generic_ids),
+            0,
+        )
 
     def test_compute_generic_ids_no_matches(self):
-        '''It should return empty recordset if there are no generic variants'''
-        test_medicament = self._new_medicament({'gpi': '2'})
-
-        self.assertFalse(test_medicament.generic_ids)
+        """ Test returns empty recordset if there are no generic variants """
+        self.assertEquals(
+            len(self.advil_us_1.generic_ids),
+            0,
+        )
 
     def test_compute_generic_ids_valid_matches(self):
-        '''It should return all matching medicaments, including self'''
-        test_medicament = self._new_medicament({'gpi': '1'})
-        test_medicament_2 = self._new_medicament({'gpi': '1'})
-        self._new_medicament({'gpi': '2'})
-
-        self.assertEqual(
-            test_medicament.generic_ids.ids,
-            [test_medicament.id, test_medicament_2.id],
+        """ Test returns all matching medicaments, including self """
+        self.advil_us_1.gpi = '1'
+        self.assertEquals(
+            len(self.advil_us_1.generic_ids),
+            1,
         )
