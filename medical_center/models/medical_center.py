16a737177f21f8d7e566d9d9ef03875dac07cbb7
Dave Lasley <dave@laslabs.com>
2017-01-10 11:38:40 -0800

    [MIG] medical_center: Migrate to v10 * Upgrade medical_center to v10

---
+++ medical_center/models/medical_center.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, models
+from odoo.modules import get_module_resource
+
+
+class MedicalCenter(models.Model):
+    _name = 'medical.center'
+    _description = 'Medical Center'
+    _inherit = 'medical.abstract.entity'
+
+    @api.model
+    def _create_vals(self, vals):
+        vals.update({
+            'is_company': True,
+            'customer': False,
+        })
+        return super(MedicalCenter, self)._create_vals(vals)
+
+    @api.model
+    def _get_default_image_path(self, vals):
+        super(MedicalCenter, self)._get_default_image_path(vals)
+        return get_module_resource(
+            'medical_center', 'static/src/img', 'medical-center-avatar.png',
+        )
