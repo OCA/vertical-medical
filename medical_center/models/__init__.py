b179322059786d20bf2be84fe96e25b3c3035cad
Dave Lasley <dave@laslabs.com>
2017-01-10 11:38:40 -0800

    [MIG] medical_center: Migrate to v10 * Upgrade medical_center to v10

---
+++ medical_center/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_center
+from . import medical_patient
+from . import res_partner
