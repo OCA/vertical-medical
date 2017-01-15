a47b455563474dfa548fac57c8398e9cb9a49279
Dave Lasley <dave@laslabs.com>
2017-01-15 15:47:14 -0800

    [ADD] medical_medicament_component: Create new module * Create new module to support in depth component analysis on medicaments

---
+++ medical_medicament_component/tests/test_medical_medicament_component.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp.tests.common import TransactionCase
+from psycopg2 import IntegrityError
+
+
+class TestMedicalMedicamentComponent(TransactionCase):
+
+    def setUp(self):
+        super(TestMedicalMedicamentComponent, self).setUp()
+        self.model_obj = self.env['medical.medicament.component']
+        self.vals = {
+            'name': 'Medicament Component',
+        }
+
+    def _new_record(self, ):
+        return self.model_obj.create(self.vals)
+
+    def test_name_unique(self):
+        self._new_record()
+        with self.assertRaises(IntegrityError):
+            self._new_record()
