64392fb68f279564842c16a2bb40e327624559ab
Dave Lasley <dave@laslabs.com>
2017-01-20 16:52:42 -0800

    Fix warnings & copyright

--- medical_pharmacy_us/__openerp__.py
+++ medical_pharmacy_us/__openerp__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Medical Pharmacy - US Locale',
-    'version': '9.0.1.0.0',
-    'author': "LasLabs, Odoo Medical Team, Odoo Community Association (OCA)",
-    'category': 'Medical',
-    'depends': [
-        'medical_pharmacy',
-    ],
+    "name": "Medical Pharmacy - US Locale",
+    "version": "9.0.1.0.0",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "category": "Medical",
     "website": "https://laslabs.com",
     "license": "AGPL-3",
-    'installable': True,
-    'auto_install': False,
+    "depends": [
+        "medical_pharmacy",
+    ],
+    "installable": True,
+    "auto_install": False,
 }
--- medical_pharmacy_us/models/medical_pharmacy.py
+++ medical_pharmacy_us/models/medical_pharmacy.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models, api
@@ -20,7 +20,7 @@ class MedicalPharmacy(models.Model):
     )
 
     @api.multi
-    @api.constrains('country_id', 'npi_num')
+    @api.constrains('npi_num')
     def _check_npi_num(self):
         """ Implement Luhns Formula to validate NPI """
         self._npi_constrains_helper('npi_num')
