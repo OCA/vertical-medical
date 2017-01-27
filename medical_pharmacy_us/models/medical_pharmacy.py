ac6d210697a3d1cd576e1b61120f3742cd531fc4
Dave Lasley <dave@laslabs.com>
2017-01-27 14:56:03 -0800

    Fix import order

--- medical_pharmacy_us/models/medical_pharmacy.py
+++ medical_pharmacy_us/models/medical_pharmacy.py
@@ -2,7 +2,7 @@
 # Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api
+from openerp import api, fields, models
 
 
 class MedicalPharmacy(models.Model):
