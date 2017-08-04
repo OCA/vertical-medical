aab5cb73dd5ff489463f172d5f507fb204454f0e
rgarnau <rgarnau20@gmail.com>
2017-08-04 12:08:12 +0200

    [ADD] Plan Definition

---
+++ workflow_plandefinition/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import workflow_type
+from . import workflow_activity_definition
+from . import workflow_plan_definition
+from . import workflow_plan_definition_action
