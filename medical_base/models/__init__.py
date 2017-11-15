0e813f8ae4600204d3cdc12a962965e93042dfe3
Enric Tobella <etobella@creublanca.es>
2017-11-15 14:22:31 +0100

    [ADD] Medical Base

---
+++ medical_base/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import medical_abstract
+from . import res_config_settings
