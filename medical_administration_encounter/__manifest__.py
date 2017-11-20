7167d45ea1820f5f01b9e40fa85ec55f14ab1ea4
rgarnau <rgarnau20@gmail.com>
2017-11-20 10:51:42 +0100

    [ADD] Medical Administration Encounter

---
+++ medical_administration_encounter/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Medical Administration Encounter',
+    'summary': 'Add Encounter concept',
+    'version': '11.0.1.0.0',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'category': 'Medical',
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'depends': [
+        'medical_administration',
+        'medical_administration_location',
+    ],
+    'data': [
+        'security/medical_security.xml',
+        'security/ir.model.access.csv',
+        'views/medical_encounter_view.xml',
+        'views/medical_menu.xml',
+        'security/ir.model.access.csv',
+        'data/medical_encounter_sequence.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
