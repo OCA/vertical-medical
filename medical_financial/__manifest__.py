b3751c37c6ed55e5ec59c0bc6bc2babc879815aa
rgarnau <rgarnau20@gmail.com>
2017-11-20 15:42:29 +0100

    [ADD] Medical Financial Menu

---
+++ medical_financial/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Medical Financial',
+    'summary': 'Medical financial',
+    'version': '11.0.1.0.0',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'category': 'Medical',
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'depends': [
+        'medical_administration'
+    ],
+    'data': [
+        'views/medical_menu.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
